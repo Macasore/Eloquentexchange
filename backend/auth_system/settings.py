7713d857214832f861ad84d27461ff407e137382
macanotmarker <macasorekingdavid@gmail.com>
2023-10-01 21:07:50 +0100

    static

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -178,8 +178,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR)
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
--- backend/build.sh
+++ backend/build.sh
@@ -6,6 +6,7 @@
 # Install Python packages from requirements.txt
 pip install -r requirements.txt
 python create_superuser.py
+python manage.py collectstatic --noinput
 # Apply database migrations
 
 
