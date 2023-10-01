f65e676cfe876ebfd3e7b2c9c9b5963892e1840d
macanotmarker <macasorekingdavid@gmail.com>
2023-10-01 19:36:24 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,18 @@
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+#!/bin/bash
+
+# Activate your virtual environment (if you're using one)
+source backend/venv/Scripts/activate
+
+# Install Python packages from requirements.txt
+pip install -r requirements.txt
+
+# Apply database migrations
+python manage.py migrate
+
+# Create a Django superuser
+python manage.py createsuperuser --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL \
+  --first_name $DJANGO_SUPERUSER_FIRSTNAME \
+  --last_name $DJANGO_SUPERUSER_LASTNAME \
+  --password $DJANGO_SUPERUSER_PASSWORD
