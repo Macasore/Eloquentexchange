c7cfd6d6b4d39095b73cf98f82f4381bbe72d458
macanotmarker <macasorekingdavid@gmail.com>
2023-10-01 22:39:49 +0100

    Update settings.py

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -178,7 +178,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static")
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
